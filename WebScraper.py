5f30322da883638258d822f6825fd5a0151c4f01
adamdonen <44457817+adamdonen@users.noreply.github.com>
2021-05-12 17:16:08 -0500

    changed to ahl stats

--- WebScraper.py
+++ WebScraper.py
@@ -5,29 +5,27 @@ import pandas as pd
 import lxml.html as lh
 
 output = csv.writer(open('test_table.csv', 'w'))
-# output.writerow(['Pos', 'Team', 'GP', 'A', 'PTS', '+/-', 'PIM', 'SHG', 'Pt/G','PIMPG', 'SH', 'SOG', 'SOA', 'SOGW', 'SO%'])
-output.writerow(['Team', 'Att', 'CMP', 'CMP%', 'YRDS/ATT',
-                 'Pass Yards', 'TD', 'INT', 'RATE', '1st', '1st%'])  # , '20', '40', 'LNG', 'SCK', 'SCKY'])
-url = "https://www.nfl.com/stats/team-stats/"
+output.writerow(['Name', 'Team', 'GP','G','A','TP','PPG','+/-'])
+# output.writerow(['Team', 'Att', 'CMP', 'CMP%', 'YRDS/ATT',
+#                  'Pass Yards', 'TD', 'INT', 'RATE', '1st', '1st%'])  # , '20', '40', 'LNG', 'SCK', 'SCKY'])
+url = "https://www.eliteprospects.com/league/ahl/stats/2020-2021"
 result = requests.get(url)
 src = result.content
 soup = BeautifulSoup(src, 'html.parser')
 table = soup.find_all("table")
-# rint(soup)
-test = table[0]
+
+test = table[1]
 teams = test.find_all("tr")
-for team in teams[1:32]:
+for team in teams[1:len(teams)-1]:
     stats = team.find_all("td")
-    name = stats[0].text
-    att = stats[1].text
-    cmp = stats[2].text
-    cmpP = stats[3].text
-    ydsPerAtt = stats[4].text
-    pYards = stats[5].text
-    td = stats[6].text
-    inter = stats[7].text
-    rate = stats[8].text
-    first = stats[9].text
-    firstPer = stats[10].text
-    output.writerow([name, att,cmp,cmpP, ydsPerAtt,pYards,td,inter,rate,first,firstPer])
+    name = stats[1].text
+    team = stats[2].text
+    gp = stats[3].text
+    g = stats[4].text
+    a = stats[5].text
+    tp = stats[6].text
+    ppg = stats[7].text
+    pim = stats[8].text
+    plus = stats[9].text
+    output.writerow([name,team, gp,g,a,tp,ppg,pim,plus])
 
